78b242aaf5cf65a56f9b3889112314dabec7050d
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-06-07 17:38:25 -0600

    Added write to file functionality for the Decrypt runner file

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -1,7 +1,6 @@
 require 'pry'
 require_relative 'enigma'
 
-
 class Encrypt < Enigma
 end
 
@@ -11,7 +10,18 @@ file.close
 
 date_today = Date.today.strftime("%d%m%y")
 
-File.write ARGV[1], Enigma.encrypt(message, Enigma.generate_key, date_today)
+decrypted_message = Enigma.decrypt(message, ARGV[2], ARGV[3])
+File.write ARGV[1], decrypted_message[:decryption]
+
+
 # p Enigma.generate_key
 # p Enigma.generate_offsets
 # Encrypt.message(message, key, date)
+
+
+#ruby decrypt.rb encrypted.txt decrypted.txt 933136 070620
+
+#ruby encrypt.rb message.txt encrypted.txt
+
+#function erase_history { local HISTSIZE=0; }
+# erase_history
