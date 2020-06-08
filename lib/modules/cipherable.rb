d4c3b41ddc39ab1530b986b27c498e8455b158b0
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-06-07 22:54:08 -0600

    Changed "encrypt" agrument in cipher method to "task".  Changed rest of method to reflect this.

--- lib/modules/cipherable.rb
+++ lib/modules/cipherable.rb
@@ -1,6 +1,6 @@
 module Cipherable
   
-  def cipher(message, key, date, encrypt)
+  def cipher(message, key, date, task)
     shifter = shift(key, date)
     decrypted_message = []
     encrypted_index = 0
@@ -10,16 +10,16 @@ module Cipherable
       if character_array.include?(char.downcase)
         plain_index = character_array.index(char.downcase)
         shifted_index = shifter.rotate(shift_counter).first
-        encrypted_index = plain_index + shifted_index if encrypt == true
-        encrypted_index = plain_index - shifted_index if encrypt == false
+        encrypted_index = plain_index + shifted_index if task == "encrypt"
+        encrypted_index = plain_index - shifted_index if task == "decrypt"
         decrypted_message << character_array.rotate(encrypted_index).first
       else
         decrypted_message << char
       end
       shift_counter += 1
     end
-    return_hash[:encryption] = decrypted_message.join if encrypt == true
-    return_hash[:decryption] = decrypted_message.join if encrypt == false
+    return_hash[:encryption] = decrypted_message.join if task == "encrypt"
+    return_hash[:decryption] = decrypted_message.join if task == "decrypt"
     return_hash[:key] = key
     return_hash[:date] = date
     return_hash
