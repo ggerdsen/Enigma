92daf80a36c889804724a98e446c5759e1618465
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-06-07 22:05:58 -0600

    Added confirmation messages to runner files, cleaned up comments

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -1,27 +1,15 @@
 require 'pry'
 require './lib/enigma'
 
-class Encrypt < Enigma
-end
+enigma = Enigma.new
 
 file = File.open('lib/'+ARGV[0], "r")
 message = file.read
 file.close
 
 date_today = Date.today.strftime("%d%m%y")
-# binding.pry
-p decrypted_message = Enigma.decrypt(message, ARGV[2], ARGV[3])
+decrypted_message = enigma.decrypt(message, ARGV[2], ARGV[3])
+
 File.write 'lib/'+ARGV[1], decrypted_message[:decryption]
 
-
-# p Enigma.generate_key
-# p Enigma.generate_offsets
-# Encrypt.message(message, key, date)
-
-
-#ruby ./lib/decrypt.rb encrypted.txt decrypted.txt 933136 070620
-
-#ruby encrypt.rb message.txt encrypted.txt
-
-#function erase_history { local HISTSIZE=0; }
-# erase_history
+puts "Created #{ARGV[1]} with the key #{decrypted_message[:key]} and date #{decrypted_message[:date]}"
--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,20 +1,17 @@
 require 'pry'
 require_relative "./enigma"
 
-
-class Encrypt < Enigma
-end
+enigma = Enigma.new
 
 file = File.open('lib/'+ARGV[0], "r")
 message = file.read
 file.close
-#
+
 date_today = Date.today.strftime("%d%m%y")
-p encrypted_message = Enigma.encrypt(message, Enigma.generate_key, date_today)
+encrypted_message = enigma.encrypt(message, enigma.generate_key, date_today)
+
 File.write 'lib/'+ARGV[1], encrypted_message[:encryption]
 
-# p Enigma.generate_key
-# p Enigma.generate_offsets
-# Encrypt.message(message, key, date)
+puts "Created #{ARGV[1]} with the key #{encrypted_message[:key]} and date #{encrypted_message[:date]}"
 
 
