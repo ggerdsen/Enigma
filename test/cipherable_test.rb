7593a193b01c97d3ab28fc9f7147c9224f3f2934
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-06-08 21:20:20 -0600

    Added test_it_can_decrypt_message test

--- test/cipherable_test.rb
+++ test/cipherable_test.rb
@@ -24,5 +24,19 @@ class CipherableTest < Minitest::Test
     expected = {:encryption=>"hmcto,rdozcl!", :key=>"23189", :date=>"060620"}
     assert_equal expected, @cryptotools.cipher(cipher_data)
   end
+  
+  def test_it_can_decrypt_message
+    key = "06678"
+    date = "060620"
+    message = "ruyiy,mtygya!"
+    task = "decrypt"
+    shifter = @cryptotools.shift(key, date)
+    crack_offset = @cryptotools.generate_offsets(date)
+    cipher_data = {ciphertext: message, key: key, date: date,
+                  shifter: shifter, task: task, crack_offset:
+                  crack_offset}
+    expected = {:decryption=>"hello, world!", :key=>"06678", :date=>"060620"}
+    assert_equal expected, @cryptotools.cipher(cipher_data)
+  end
 
 end
