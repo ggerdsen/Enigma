f80c37d4938f2b5d7a6fa2bf7df5edd029383c67
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-06-08 20:59:13 -0600

    Added test_it_can_encrypt_message test

--- test/cipherable_test.rb
+++ test/cipherable_test.rb
@@ -9,7 +9,20 @@ class CipherableTest < Minitest::Test
 
   def test_it_exists
     assert_equal Module, Cipherable.class
-  end  
+  end
 
+  def test_it_can_encrypt_message
+    key = "23189"
+    date = "060620"
+    message = "Hello, world!"
+    task = "encrypt"
+    shifter = @cryptotools.shift(key, date)
+    crack_offset = @cryptotools.generate_offsets(date)
+    cipher_data = {ciphertext: message, key: key, date: date,
+                  shifter: shifter, task: task, crack_offset:
+                  crack_offset}
+    expected = {:encryption=>"hmcto,rdozcl!", :key=>"23189", :date=>"060620"}
+    assert_equal expected, @cryptotools.cipher(cipher_data)
+  end
 
 end
